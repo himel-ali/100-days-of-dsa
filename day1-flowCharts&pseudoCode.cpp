2285f9037764109507826f2fb73d0378f78a752b
himel-ali <himelali241@gmail.com>
2026-05-10 01:52:09 +0530

    pushed

--- day1-flowCharts&pseudoCode.cpp
+++ day1-flowCharts&pseudoCode.cpp
@@ -1,18 +1,18 @@
-## Day 1
-Topics Covered:
-- Introduction to Programming
-- Flowcharts
-- Pseudocode
-- Basic Mathematics in Programming
+// ## Day 1
+// Topics Covered:
+// - Introduction to Programming
+// - Flowcharts
+// - Pseudocode
+// - Basic Mathematics in Programming
 
-Problems:
-- Sum of two numbers a & b
-- Area of Square
-- Minimum of 2 Numbers
-- Is number odd or even
-- Sum of numbers from 1 to n
-- If number n is prime or not
-- Calculate simple intrest
-- calculate max of 2 numbers
-- calculate factorial of a number N
-- Given a person's age, find if they should get a licence or not
+// Problems:
+// - Sum of two numbers a & b
+// - Area of Square
+// - Minimum of 2 Numbers
+// - Is number odd or even
+// - Sum of numbers from 1 to n
+// - If number n is prime or not
+// - Calculate simple intrest
+// - calculate max of 2 numbers
+// - calculate factorial of a number N
+// - Given a person's age, find if they should get a licence or not
