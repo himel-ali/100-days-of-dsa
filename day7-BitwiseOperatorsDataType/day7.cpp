a8f4b46fd73bec998d083eda961d96def203f16f
himel-ali <himelali241@gmail.com>
2026-05-13 06:30:28 +0530

    Pushing progree

---
+++ day7-BitwiseOperatorsDataType/day7.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+// Bitwise Operators
+
+// Bitwise & (And) -> 0's presence will make it 0
+                // 1 & 0 -> 0
+                // 0 & 1 -> 0
+                // 0 & 0 -> 0
+                // 1 & 1 -> 1
+
+// Bitwise | ( OR ) -> presence of 1 will make it 1 
+                // 1 | 0 -> 1
+                // 0 | 1 -> 1
+                // 0 | 0 -> 0
+                // 1 | 1 -> 1
+
+// Bitwise ^ ( XOR ) -> if biits are same -> 0 if bits diff -> 1
+                // 1 ^ 0 -> 1
+                // 0 ^ 1 -> 1
+                // 0 ^ 0 -> 0
+                // 1 ^ 1 -> 0
+
+// Bitwise << (Left shift operator) -> 
+
+// Bitwise >> (Right shift operator) ->
+
+// Operator precedence 
+
+// Scope Local And Global  ------------>  Vars accecable in which parts of the code.
+
+// Data type modifiers 
+// long ----> greater than equal to 4 bytes
+// short ---> modifies data types to 2 bytes
+// long long ---> modifies data types to 
+// signed -----> 
+// Unsigned ---> 
+
+
+
+
+int main () {
+
+
+cout<< sizeof( long int);
+cout<< sizeof( short int);
+cout<< sizeof( long long int);
+cout<< sizeof( long int);
+
+
+
+    return 0;
+}
