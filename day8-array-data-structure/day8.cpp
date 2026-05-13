711d5355df6deb6116bcc02f88d73e52c5af84b8
himel-ali <himelali241@gmail.com>
2026-05-13 10:21:00 +0530

    Pushing Progress

--- day8-array-data-structure/day8.cpp
+++ day8-array-data-structure/day8.cpp
@@ -12,10 +12,14 @@ using namespace std;
         // dataType nameOfArray [size of array];
         // int marks [5];
 
+
 // Position of Arrays 
  // stars at 0th index, o, 1, 2 , 3, 4;
 
+
 // Pass by reference ----> 
+
+
 
 // int smallestN () {
 //     int smallest = INT_MAX;
@@ -23,7 +27,14 @@ using namespace std;
 //     for ( int i = 0; i < )
 // }
 
+
+
+
 // Linear search ---------->
+
+
+
+
 
 //  int linearSearch(int arr[], int size, int target ){
 //     int idx = 0;
@@ -36,10 +47,122 @@ using namespace std;
 //         return -1;
 //  }
 
+
+
+
+
+
 void reverseA(int arr[], int size){
     for (int i = 0, j= size-1; i<j; i++, j--){
         swap(arr[i], arr[j]);
     }}
+
+
+// WAF to calculatw the summ and product of all numbers in an array.
+
+int sumArray(int arr[], int size){
+    int sum = 0;
+    for (int i = 0; i<= size-1; i++){
+        sum += arr[i];
+    }
+    return sum;
+}
+
+
+int productArray(int arr[], int size){
+    int product = 1;
+    for (int i = 0; i<= size-1; i++){
+        product *= arr[i];
+    }
+    return product;
+}
+
+
+
+
+// WAF find min number in an array
+
+int minInArray(int arr[], int size){
+    int smallest = INT_MAX;
+    int idx=0;
+    for (int i = 0; i<size;i++){
+        if (arr[i]<smallest){
+            smallest = arr[i];
+             idx = i;
+        }
+    }
+    return idx;
+}
+
+
+
+// WAF find max number in an array
+
+int maxInArray(int arr[], int size){
+    int largest = INT_MIN;
+    int idx=0;
+    for (int i = 0; i<size;i++){
+        if (arr[i]>largest){
+            largest = arr[i];
+            idx = i;
+        }
+    }
+    return idx;
+}
+
+
+
+
+// WAF to swap the max and min number of an array
+
+void swapMaxMin(int arr[], int size) {
+    int max_index = maxInArray(arr, size);
+    int min_index = minInArray(arr, size);
+    swap(arr[max_index],arr[min_index]);
+}
+
+
+
+// WAF to print all the unique values in an array
+
+
+int unique(int arr[], int size){
+
+}
+
+
+
+
+
+//  WAF to print intersections of 2 arrays
+
+
+int intersection(int arr1[], int size1, int arr2[], int size2){ 
+    for ( int i = 0; i<size1; i++){
+        for (int j = 0 ; j<size2 ; j++){
+            if (arr1[i] == arr2[j]){
+                return arr1[i];
+            }
+        }
+    }
+    return -1;
+}
+
+
+
+
+// WAF to print all unique values in an array
+
+
+
+
+
+
+
+
+
+
+
 
 int main() {
 
@@ -55,6 +178,11 @@ int main() {
     //     cout<< markss[i]<<endl;
     // }
 
+
+
+
+
+
     // Find the smallest and largest number in an array
 
     // int array[6] = {5, 15, 22, 1, -15, 24};
@@ -68,6 +196,11 @@ int main() {
     //     }
     // }
     // cout << smallest << endl;
+
+
+
+
+
 
     // // Find the smallest and largest number in an array
     // int largest = INT_MIN;
@@ -83,8 +216,19 @@ int main() {
     // cout <<idx<< endl ;
     // cout << largest << endl;
 
+
+
+
+
+
     
     // cout<< linearSearch(array, 7, 8)<< endl;
+
+
+
+
+
+
 
     //Reverse an array
     int array [] = {1,2,3,4};
