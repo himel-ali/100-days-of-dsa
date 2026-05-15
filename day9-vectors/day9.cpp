5c6551afcbafdf52475c9cb7a91293ad349e5510
himel-ali <himelali241@gmail.com>
2026-05-15 07:08:33 +0530

    pushing progress

---
+++ day9-vectors/day9.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+// Vectors -> are like arrays, however dynamic in nature, can be of flaxible range. include in terminal -st=c++11 after g++.
+
+//Vector syntax 
+
+// vector <DataTypeOfVector> vec;
+
+
+
+int main (){
+    vector <int> vec; /// Vector is defined here however it is a empty vector, size is 0.
+    vector <int> vec2 = {1,2,3};
+    vector <char> vec3 = {'a','b','c','d','e','f','g'};
+    vector<int> vec4(3,0); // nameOfvector(size of vec, the repeated element in the vector)
+    cout<< vec2.size()<< endl;
+    
+
+    // For Each Loop
+    for (char i : vec3){ /// initiater is not storing the index here, it is storing the value in the index of the vector.
+        cout<<i<<endl;
+    }
+
+
+    // Vector Function
+    
+    // size;
+    cout<< "size "<< vec3.size()<< endl;
+
+    // push_back
+    vec.push_back(25);
+    cout<< "Size after push_bcak "<< vec.size()<< endl;
+
+    // pop_back ---> ladt element will be deleted
+    vec.pop_back();
+    cout<< "Size after push_bcak "<< vec.size()<< endl;
+
+    // fornt() ---> We are trying to print the first value of vector
+    cout<< "Front "<< vec3.front()<<endl;
+
+    // back() ---> We are trying to print the last value of vector
+    cout<< "Back "<< vec3.back()<<endl;
+
+    // at() ---> gives out the value of index that we provide.
+    cout<< "at "<< vec3.at(3)<<endl;
+
+
+
+    return 0;
+}
