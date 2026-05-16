5dab445a71b13facecf57d0275d078fe2fb9746e
himel-ali <himelali241@gmail.com>
2026-05-17 03:10:52 +0530

    understanding and implementing time and space complexity of DSA solutions

--- day12-time-and-space-complex/day12.cpp
+++ day12-time-and-space-complex/day12.cpp
@@ -8,6 +8,11 @@ using namespace std;
 // Time and Space Complexity
 // - Not the actual time taken 
 // - but the amount of time taken as function of input size (n)
+// Big O Notatiion (uperboud)  O(n)  ---> is for the worst case scenario when the operation is running thorugh all the elements to the end of the data structure
+
+// worst case sceanrio ---> O(big o), upper bound
+// avg case scenario --->  Θ(theta)
+// best case scenario --->  Ω(omega), lower bound
 
 
 
