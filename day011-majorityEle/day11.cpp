630d9c7af249c43079970e6e14079f35bdb38c75
himel-ali <himelali241@gmail.com>
2026-05-16 10:46:20 +0530

    Pushing Progress

---
+++ day011-majorityEle/day11.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+vector<int> pairSumB(vector<int>& nums, int target){
+    vector<int> ans;
+    int size = nums.size();
+    for (int i=0; i<size;i++){
+        for (int j=i+1; j<size;j++){
+            if (nums[i]+nums[j]==target){
+                ans.push_back(i);
+                ans.push_back(j);
+                return ans;
+            }
+        }
+    }
+    return {};
+
+}
+
+int main (){
+    vector<int> vec ={2,7,15,18};
+    vector <int> ans = pairSumB(vec, 22);
+    for (int i = 0 ; i<ans.size();i++){
+        cout << ans[i]<<" ";
+    }
+
+
+
+
+
+
+    return 0;
+}
