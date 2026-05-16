e8ccb49da69bbe7070c2300efb3d9effd91ea85c
himel-ali <himelali241@gmail.com>
2026-05-17 03:38:37 +0530

    understanding and implementing time and space complexity of DSA solutions

--- day12-time-and-space-complex/day12.cpp
+++ day12-time-and-space-complex/day12.cpp
@@ -5,7 +5,7 @@
 #include <climits>
 using namespace std;
 
-// Time and Space Complexity
+// Time Complexity
 // - Not the actual time taken 
 // - but the amount of time taken as function of input size (n)
 // Big O Notatiion (uperboud)  O(n)  ---> is for the worst case scenario when the operation is running thorugh all the elements to the end of the data structure
@@ -14,8 +14,21 @@ using namespace std;
 // avg case scenario --->  Θ(theta)
 // best case scenario --->  Ω(omega), lower bound
 
+// Space complexity
+// Amount of space taken by an algoritm as function of input size(n).
+// space can be ---> vect/array/string
+//                  auxillary space(extra) ---> space taken by output array
+// if the output space is constant then space compx is O(k)/O(1) 
+// if the output space is same as the input size then space compx is O(n) 
 
 
+
+// Time compx is more important than space compx
+// Common time compx
+// O(1)/O(k) ---> constant compx, no loop no recurssion
+// O(n) ---> linear, fibonacci, N factorial, Kadane's(one loop)
+// O(n^2), O(n^3) ---> bubble sort, selection sort, insertion sort(2 loops for n^2, 3 loops foor n^3)
+// O(logn) ---> binary search
 
 
 
