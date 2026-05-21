6e7790cc059dfb416bbdb4dc9f2c30c59941ada3
himel-ali <himelali241@gmail.com>
2026-05-21 15:37:25 +0530

    Understanding and working on binary search

--- day17-binarySearch/day177.cpp
+++ day17-binarySearch/day177.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
-#include<vector>
+#include <vector>
 #include <climits>
 #include <algorithm>
+using namespace std;
 
 
 // Binary search --->  Like finding words in a dictnory
@@ -14,7 +15,25 @@
 // we find the mid first
 // mid = (st + end)/2
 
+int binarySearch(vector <int>& nums, int target){
+    
+    int st = 0;
+    int end = nums.size()-1; 
+   
+    while( st <= end ){
+        int mid = (st + end )/2;
+        
+        if ( nums[mid] == target){
+            return mid;
+        } else if (nums[mid]>target){
+            end = mid-1;            
+        } else  if (nums[mid]<target){
+            st = mid+1;
+        }    
+    }
 
+    return -1;
+}
 
 
 
