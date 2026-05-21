7a7a4a47b16262ff3c89d94b41b8e76c3f2e3f25
himel-ali <himelali241@gmail.com>
2026-05-21 15:18:21 +0530

    Understanding and working on binary search

---
+++ day17-binarySearch/day177.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include<vector>
+#include <climits>
+#include <algorithm>
+
+
+// Binary search --->  Like finding words in a dictnory
+// condition already applied ---> when the array is sorted(monotonic/follows monotounos function)
+
+
+// example --->
+// arr[] = {-1, 0,3,4,5,9,12}, sorted in ascending order.
+// target = 12
+// we find the mid first
+// mid = (st + end)/2
+
+
+
+
+
+
+int main (){
+
+    int arr[] = {-1, 0,3,4,5,9,12};
+
+
+
+    return 0;
+}
