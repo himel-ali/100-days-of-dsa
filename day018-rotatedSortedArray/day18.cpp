c45d6bda7fb09da2702f5987c7c1168cec209a97
himel-ali <himelali241@gmail.com>
2026-05-24 10:01:08 +0530

    Learning and applying Rotated sorted Array

--- day018-rotatedSortedArray/day18.cpp
+++ day018-rotatedSortedArray/day18.cpp
@@ -7,32 +7,34 @@ using namespace std;
 // Search in Rotated sorted array --->
 
 int reverseSortedBS(vector<int> nums, int target){
-    int st = 0, end = nums.size();;
-    int mid = st +( end - st )/2;
+    int st = 0, end = nums.size()-1;
 
-    for  ( ; st>  end; ){
-        if ( mid == target){
+    for  ( ; st <=  end; ){
+        int mid = st +( end - st )/2;
+        if ( nums[mid] == target){
             return mid;
         }else if(nums[mid] >= nums[st]){
-            if (nums[st] <= target <= nums[mid]){
+            if (target >= nums[st] && target <= nums[mid] ){
                 end = mid -1;
             }else{
                 st = mid + 1;
             }
         }else {
-            if (nums[st] <= target <= nums[mid]){
-                end = mid -1;
-            }else{
+            if (target >= nums[mid] && target <= nums[end]){
                 st = mid + 1;
+            }else{
+                end = mid - 1;
             }
         }
     }
-
-
+    return -1;
 }
 
 
 int main (){
+    vector<int> vec = {3,4,5,6,7,0,1,2};
+    cout<< "Go"<< endl;
+    cout<< reverseSortedBS(vec, 0)<<endl;
 
     
 
