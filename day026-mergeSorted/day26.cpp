418a3e42b7f40089993b6b0b0c391714aa000403
himel-ali <himelali241@gmail.com>
2026-06-02 07:01:01 +0530

    Merge Sorting Algorithms

--- day026-mergeSorted/day26.cpp
+++ day026-mergeSorted/day26.cpp
@@ -5,7 +5,36 @@
 using namespace std;
 
 
+void mergeSorted(vector<int>& nums1,vector<int>& nums2, int m, int n){
+    int swapper = m+n-1;
+    int lastOfBig = m-1;;
+    int lastOfSmall = n -1;
+    
+    for (; swapper >= 0;){
+        if (nums1[lastOfBig]>nums2[lastOfSmall]){
+            swap(nums1[lastOfBig],nums1[swapper]);
+            lastOfBig--;
+            swapper--;
+        }else {
+            swap(nums2[lastOfSmall], nums1[swapper]);
+            lastOfSmall--;
+            swapper--;
+        }
+        if (lastOfSmall < 0) break;
+    }
+
+
+}
+
+
 int main (){
+    vector <int> nums1 ={1,2,3,0,0,0};
+     vector <int> nums2 ={2,5,6};
+
+     mergeSorted(nums1,nums2,3,3);
+     for (int i = 0 ; i<6;i++){
+        cout<< nums1[i]<<" ";
+     }
 
 
 
