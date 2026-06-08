407736e145347e2e46b7a0a0addb221a3edc71b4
himel-ali <himelali241@gmail.com>
2026-06-08 12:19:50 +0530

    Learning about CPP STL

--- day027-mergeSorted/day27.cpp
+++ day027-mergeSorted/day27.cpp
@@ -16,8 +16,19 @@ using namespace std;
 //          .at() or [] ---> vec[i] is same as vec.at(i)
 //          front ---> to acess the front element
 //          back ---> to acess the last element 
-//          erase ---> erase some element or a range of elements, we can pass the iterator - vec.erase(vec.begin() + 2) (for ele at second index)
-//          insert --->   
+//          erase ---> erase some element or a range of elements, we can pass the iterator - vec.erase(vec.begin() + 2, vec.begin() +  4) (for ele at second index) | this caan not change the capacity of the vector
+//          insert --->   We can insert an element whereevr we want in the vector - vec.insert(vec.begin()+2, 100)
+
+//          clear ---> clears the vector but the capacity remains the same
+
+//          empty ---> if our vector is empty, gives out a bool result
+
+// Iterators --->
+//          vec.begin() ---> points the first element of the vec
+
+//          vec.end() ---> points the next to last element of the vec, whuch is garbage value
+
+//          vec.rbegin() --->              
 
 
 
@@ -26,6 +37,10 @@ using namespace std;
 // vector <int> vec = {1,2,3};
 // vector <int> vec =  (10,2);
 // vector <int> vec2 =(vec1);
+
+
+// 2. List --->
+//
 
 
 // Iterators
