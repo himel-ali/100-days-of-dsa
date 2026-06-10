cff610ce5a324f4bdcc101aa4bb07b7575aefbb1
himel-ali <himelali241@gmail.com>
2026-06-10 08:48:22 +0530

    understanding and working on strings

--- day029-palinRemoveAll/day29.cpp
+++ day029-palinRemoveAll/day29.cpp
@@ -16,10 +16,31 @@ int i = 0, j = s.size()-1;
     return true;
 }
 
+///   daabcbaabcbc
+
+string remOc1( string s, string t){
+
+    for (int j = 0; j<s.size(); j++){
+            if (s.substr(j,t.size()) == t){
+                s.erase(j,t.size());
+                j=0;
+            }
+    }
+
+return s;
+}
+
+string remOc( string s, string part){
+    while (s.length() > 0 && s.find(part)<s.length()){
+        s.erase(s.find(part),part.length());
+    }
+    return s;
+}
+
 
 int main () {
-     cout<< palindrome("A man, a plan, a canal: Panama");
-
+     cout<< palindrome("A man, a plan, a canal: Panama")<<endl;
+    cout<< remOc("daabcbaabcbc", "abc");
 
     return 0;
 }
