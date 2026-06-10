e287d1947e7602ef509d3665e02d2489ab763f41
himel-ali <himelali241@gmail.com>
2026-06-10 10:19:40 +0530

    permutation on strings

--- day030-permutationInString/day30.cpp
+++ day030-permutationInString/day30.cpp
@@ -2,6 +2,25 @@
 #include <string>
 using namespace std;
 
+bool checkInc (string s1, string s2){
+
+    int s1Feq[26]= {0};
+
+    for (int i = 0; i<s1.length() ; i++){
+        s1Feq[s1Feq[i]-'a']++;
+    }
+
+    int s2Size = s2.length();
+
+    for(int i=0; i<s2.length(); i++){
+        int s2Idx = i;
+        
+
+
+
+    }
+}
+
 
 
 
