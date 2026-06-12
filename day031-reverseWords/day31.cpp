384b3ec08b905186d5d5ddc149046c488737817e
himel-ali <himelali241@gmail.com>
2026-06-12 11:01:56 +0530

    Working on string problems Word reverse in String

--- day031-reverseWords/day31.cpp
+++ day031-reverseWords/day31.cpp
@@ -3,36 +3,30 @@
 #include <algorithm>
 using namespace std;
 
-
-
-
 string revWord(string s){
+
     string ans= "";
     reverse(s.begin(), s.end());
 
-   
-    for (int i = 0; i<s.length(); i++){
-    string word = "";
-
+    
+    for (int i = 0; i <s.length(); i++){
+        string word = "";
     while (i<s.length() && s[i]!=' '){
         word+= s[i];
         i++;
-    }
+        }
     reverse(word.begin(), word.end());
     
     if (word.length()>0 ){
-        ans+= " "+word;
+        ans+= " " + word;
+        }
     }
-    }
-    return ans;
-
+    return ans.substr(1);
 }
 
 
 int main () {
     cout<< revWord("the sky is blue");
-
-
 
     return 0;
 }
