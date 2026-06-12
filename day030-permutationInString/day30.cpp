25a5e3e9b9ec2c65c6210ef9a0148f70f654cd17
himel-ali <himelali241@gmail.com>
2026-06-12 09:38:07 +0530

    Understanding and applying string problems

--- day030-permutationInString/day30.cpp
+++ day030-permutationInString/day30.cpp
@@ -7,18 +7,28 @@ bool checkInc (string s1, string s2){
     int s1Feq[26]= {0};
 
     for (int i = 0; i<s1.length() ; i++){
-        s1Feq[s1Feq[i]-'a']++;
+        s1Feq[s1[i]-'a']++;
     }
 
-    int s2Size = s2.length();
+    int windowSize = s1.length();
 
     for(int i=0; i<s2.length(); i++){
         int s2Idx = i;
+        int windowIdx = 0;
+        int s2Feq [26] ={0};
+
+        while (windowIdx< windowSize){
+            s2Feq[s2[s2Idx]-'a']++;
+            s2Idx++;
+            windowIdx++;
+        }
+
+        if (equal(s1Feq, s1Feq+26, s2Feq)){
+            return true;
+        }
         
-
-
-
     }
+    return false;
 }
 
 
