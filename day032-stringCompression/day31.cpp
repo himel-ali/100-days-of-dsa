961e2346c51eebb02fea88b17871a0c59d2ffdf4
himel-ali <himelali241@gmail.com>
2026-06-15 10:03:52 +0530

    String compression LC443

--- day032-stringCompression/day31.cpp
+++ day032-stringCompression/day31.cpp
@@ -2,7 +2,40 @@
 using namespace std;
 
 
-//443 String compresion
+//LC443 String compresion
+
+int compre(vector<char>& chars){
+    
+    for (int i = 0 ;i< chars.size(); i++ ){
+        int occur = 1;
+
+        for (int j=i;j + 1 < chars.size() && chars[j]==chars[j+1];j++){
+                occur++;   
+        }
+        chars.push_back(chars[i]);
+        if(occur>1){
+        chars.push_back(occur);
+        }
+    }
+}
+
+
+
+
+int compress(vector<char>& chars){
+    int idx = 0 ;
+    int write = 0;
+    
+    while (idx < chars.size()){
+        int occur = 1;
+
+        while(idx + 1 < chars.size()  && chars[idx]==chars[idx+1]){
+            occur++;
+            idx++;
+        }
+        idx++;
+    }
+}
 
 
 
