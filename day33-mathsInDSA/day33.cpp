b11da32440496388faa26acc95c6bb840b631cf5
himel-ali <himelali241@gmail.com>
2026-06-15 11:50:05 +0530

    Maths in DSA

--- day33-mathsInDSA/day33.cpp
+++ day33-mathsInDSA/day33.cpp
@@ -1,26 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-    bool isPrime(int n){
-        
-        if(n<=1){
-            return false;
-        }
-    for (int i = 2; i<n/2; i++){
-            if(n%i==0){
-                return false;
-            }
-        }
-        return true;
-    }
 
 int countPrime (int n){
-    for (int i = 2; i<n; i++){
+    vector <bool> isPrime (n+1, true ); 
+    int count = 0;
 
-
+     for (int i = 2; i * i <= n; i++) {
+        if (isPrime[i]) {
+            count++;
+            for (int j = i * i; j <= n; j += i) {
+                isPrime[j] = false;
+            }
+        }
     }
-
-
+    return count;
 }
 
 int main() {
