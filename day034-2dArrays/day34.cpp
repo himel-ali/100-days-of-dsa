d81414aa209748b6c8c9bfff37c0f5ead3cd632e
himel-ali <himelali241@gmail.com>
2026-06-19 08:35:32 +0530

    2d Arrays in CPP

--- day034-2dArrays/day34.cpp
+++ day034-2dArrays/day34.cpp
@@ -1,6 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+pair<int, int> searchArray(int matrix[][5], int row, int col, int tar ){
+    for (int i = 0; i<row; i++){
+        for (int j = 0; j<col; j++){
+            if(matrix[i][j]==tar){
+                return {i,j};
+            }
+        }
+    }
+    return {-1,-1};
+}
+
+
+
+int maxRowSum(int matrix[4][5],int row, int col){
+    int maxSum = INT_MIN;
+    int  maxRow = 0;    
+
+    for (int i= 0; i < row; i++){
+    int rowSum = 0;
+        for (int j= 0; j<col; j++){
+            rowSum += matrix[i][j];
+        }
+        if(rowSum > maxSum){
+            maxSum =rowSum;
+            maxRow = i;            
+        }
+    }
+    return maxRow;
+}
 
 
 int main (){
@@ -8,22 +37,23 @@ int main (){
     // int nameOfMatrix [row] [column];
     // int matrix[4][5];
 
-    int matrix[4][5] = {{1,2,3,4,5}, {5,4,3,2,1}, {0,3,4,1,1}, {6,7,8,9,10}};
+    int matrix[4][5] = {{1,2,3,4,5}, {5,400,3,2,1}, {0,3,4,1,1}, {6,33,8,9,10}};
 
     // we track cell to acess the data
     // nameOfMatrix [row num] [col num]
     //row/col starts at 0, just like index
 
-    cout<< matrix[2][2]<<endl;
+    // cout<< matrix[2][2]<<endl;
 
-    for(int i = 0;i<4;i++){
-        for(int j= 0; j<5; j++){
-            cout<< matrix[i] [j]<<" ";
-        }
-        cout<< endl;
-    }
+    // for(int i = 0;i<4;i++){
+    //     for(int j= 0; j<5; j++){
+    //         cout<< matrix[i] [j]<<" ";
+    //     }
+    //     cout<< endl;
+    // }
+    // auto result = searchArray(matrix, 4, 5, 33);
+    // cout << "("<<result.first <<","<< result.second <<")" <<endl;;
 
-
-
+    cout<< maxRowSum(matrix,4,5)<<endl;
     return 0;
 }
