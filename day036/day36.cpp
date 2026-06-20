1704b84acb0f7f692074129a1ca6be79e6e94408
himel-ali <himelali241@gmail.com>
2026-06-21 01:58:37 +0530

    Spiral Matrix LC54

--- day036/day36.cpp
+++ day036/day36.cpp
@@ -2,10 +2,45 @@
 using namespace std;
 
 
+vector<int> spiralOrder(vector<vector<int>>& matrix){
+
+    int rows = matrix.size(), cols =matrix[0].size();
+    int srow = 0, erow = rows-1;
+    int scol = 0, ecol = cols -1;
+
+    vector<int> ans;
+
+    while(srow <= erow && scol <= ecol){
+    // top
+        for (int i = scol; i<=ecol; i++){
+            ans.push_back(matrix[srow][i]);
+        }
+    // right 
+        for (int j = srow+1; j<= erow; j++){
+            ans.push_back(matrix[j][ecol]);
+        }
+    //bottom
+    if (srow != erow){
+        for(int i = ecol-1; i>=scol; i--){
+             ans.push_back(matrix[erow][i]);
+        }
+    }
+    //left 
+    if (scol != ecol){
+        for (int j = erow-1; j>=srow+1; j--){
+            ans.push_back(matrix[j][scol]);
+        }
+    }
+        srow++; erow--; scol++; ecol--;
+    }
+    return ans;
+}
+
+
+
+
 int main (){
 
-
-    cout << "Push"<<endl;
 
 
 
