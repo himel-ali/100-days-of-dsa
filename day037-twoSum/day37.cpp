4e9403309f1e35a5725dc14920defaa7c4037168
himel-ali <himelali241@gmail.com>
2026-06-21 09:09:08 +0530

    2Sum LC1

--- day037-twoSum/day37.cpp
+++ day037-twoSum/day37.cpp
@@ -2,6 +2,35 @@
 using namespace std;
 
 
+// 2 sum
+// Brute force ---> O(n^2)
+
+// Optimised --- O(nlogn)
+// can not use sorting, since it will destroy the  indices
+
+// Optimized ---> Hash Map
+
+ vector<int> twoSum(vector<int>& nums, int target){
+
+    unordered_map<int, int> m;
+    vector<int> ans;
+
+    for (int i = 0; i<nums.size(); i++){
+        int first = nums[i];
+        int sec = target - first;
+
+        if(m.find(sec) != m.end()){
+            return {m[sec], i};
+        }
+        m[nums[i]] = i;
+    }
+
+    return {};
+ }
+
+
+
+
 int main (){
 
 
