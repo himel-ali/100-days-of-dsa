9becf012bafdc7d2a332df6ee0be8b033a29b7f3
himel-ali <himelali241@gmail.com>
2026-06-23 15:31:25 +0530

    3sum

--- day038-3Sum/day38.cpp
+++ day038-3Sum/day38.cpp
@@ -8,14 +8,22 @@ using namespace std;
     // 2. find the unique by using sorting
 
 vector<vector<int>> threeSumB(vector<int>& nums){
-    vector<int> ans;
     int n = nums.size();
+    vector<vector<int>> ans;
+
+    set<vector<int>> s;
 
     for (int i = 0; i<n ; i++){
         for (int j=i+1; j<n ; j++){
             for (int k = j+1 ; k<n; k++){
                 if(nums[i] + nums[j] + nums[k] == 0){
-                    ans.insert({nums[i], nums[j], nums[k]});
+                    vector<int>  trip = {nums[i], nums[j], nums[k]};
+                    sort(trip.begin(), trip.end());
+
+                    if(s.find(trip) == s.end()) {
+                        s.insert(trip);
+                        ans.push_back(trip);
+                    }
                 }
             }
         }
