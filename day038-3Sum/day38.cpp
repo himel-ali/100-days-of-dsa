81fdcdb4e8896340f2a784e2dc71a5ce6ea2d39d
himel-ali <himelali241@gmail.com>
2026-06-23 15:16:42 +0530

    3sum

--- day038-3Sum/day38.cpp
+++ day038-3Sum/day38.cpp
@@ -7,6 +7,20 @@ using namespace std;
     // 1. find all the triplates
     // 2. find the unique by using sorting
 
+vector<vector<int>> threeSumB(vector<int>& nums){
+    vector<int> ans;
+    int n = nums.size();
+
+    for (int i = 0; i<n ; i++){
+        for (int j=i+1; j<n ; j++){
+            for (int k = j+1 ; k<n; k++){
+                if(nums[i] + nums[j] + nums[k] == 0){
+                    ans.insert({nums[i], nums[j], nums[k]});
+                }
+            }
+        }
+    }
+}
 
 
 
