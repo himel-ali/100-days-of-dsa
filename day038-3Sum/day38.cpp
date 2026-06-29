3b63447d0fc6788db15ed7366cf773b2073cb703
himel-ali <himelali241@gmail.com>
2026-06-29 11:51:14 +0530

    3Sum

--- day038-3Sum/day38.cpp
+++ day038-3Sum/day38.cpp
@@ -65,6 +65,38 @@ vector<vector<int>> threeSum(vector<int>& nums) {
 }
 
 
+vector<vector<int>> threeSum(vector<int>& nums){
+    sort(nums.begin(), nums.end());
+    vector<vector<int>> ans;
+    int n = nums.size() -1;
+
+    for (int i = 0; i<n; i++){
+        if(i>0 && nums[i]==nums[i-1]) continue;
+        int j = i + 1;
+        int k = n - 1;
+
+        while(j<k){
+            int sum = nums[i]+nums[j]+nums[k];
+
+            if(sum > 0){
+                k--;
+            }else if(sum < 0){
+                j++;
+            }else{
+                ans.push_back({nums[i], nums[j], nums[k]});
+
+                while(j > k && nums[j] == nums[j+1]) j++;
+                while(j > k && nums[j] == nums[j+1]) k--;
+
+                j++, k--;
+            } 
+        }
+
+    }
+    return ans;
+}
+
+
 
 int main (){
 
