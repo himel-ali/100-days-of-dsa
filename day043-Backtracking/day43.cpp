754732717cfbf1911ccdd93add847cda9f8e733c
himel-ali <himelali241@gmail.com>
2026-07-13 14:30:48 +0530

    Recursion

--- day043-Backtracking/day43.cpp
+++ day043-Backtracking/day43.cpp
@@ -1,6 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+vector<int> pS(vector<int> arr,vector<int> ans, int i){
+    if(i == arr.size()) return ans;
+
+    ans.push_back(arr[i]);
+    return pS(arr, ans, i + 1);
+
+    ans.pop_back();
+    return pS(arr, ans, i + 1);
+
+}
+
 
 int main () {
 
